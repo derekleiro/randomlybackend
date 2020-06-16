d288a733933824dd0d07be1012b6ffdcdc633744
Derek Leiro <derekleiro@gmail.com>
2020-06-16 16:48:22 +0300

    Messenger endpoints

---
+++ models/chat-room.js
@@ -0,0 +1,21 @@
+const moongoose = require("mongoose");
+
+const ChatRoom = new moongoose.Schema({
+    senderUID: {type: String, required: true},
+    receiverUID: {type: String, required: true},
+    createdAt: {type: Number, required: true},
+    senderInfo: {
+        profilePicture: {type: String, required: true},
+        profileName: {type: String, required: true},
+    },
+    recieverInfo: {
+        profilePicture: {type: String, required: true},
+        profileName: {type: String, required: true},
+    },
+    lastMessage: {type: String, required: true},
+    senderUnread: {type: Number, required: true},
+    recieverUnread: {type: Number, required: true},
+    accepted: {type: Boolean, required: true},
+});
+
+module.exports = moongoose.model("Chatroom", ChatRoom);
